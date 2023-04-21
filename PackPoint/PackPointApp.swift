c80d0ff0b8cb66920402bfad85b4b3db29f436d6
Skeegan123 <mcfeegan123@gmail.com>
2023-04-21 11:10:04 -0600

    Add local files

---
+++ PackPoint/PackPointApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackPointApp.swift
+//  PackPoint
+//
+//  Created by Keegan Gaffney on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PackPointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
